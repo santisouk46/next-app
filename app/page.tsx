ab13b9831f8fb052295245ccd5d771caf2a8706c
jecko78 <jecko78@gmail.com>
2026-07-21 22:07:36 +0700

    change background color to #880088

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        Savannakhet University Test Page In The Second Time
+        Savannakhet University Test Page In The Third Time
       </main>
   );
 }
